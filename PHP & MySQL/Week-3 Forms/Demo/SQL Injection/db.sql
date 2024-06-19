8a4f60ddf57e263eae280448995f16b1f7853ae6
Jaustin321 <145405449+Jaustin321@users.noreply.github.com>
2024-06-19 17:51:37 -0600

    Delete PHP & MySQL/Week-3 Forms/Demo/SQL Injection directory
    Moved to week 6

--- PHP & MySQL/Week-3 Forms/Demo/SQL Injection/db.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(30) NOT NULL,
-    password VARCHAR(30) NOT NULL
-);
-
-INSERT INTO users (username, password) VALUES ('admin', 'admin123');
