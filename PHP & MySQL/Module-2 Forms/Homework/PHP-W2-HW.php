05b8a9e2eb3cfdc696df253c9e8546bfae137fa4
Joshuaaustin-dev <austin.j0sh135@gmail.com>
2025-11-14 15:19:49 -0700

    reorganized & Renamed PHP Files

---
+++ PHP & MySQL/Module-2 Forms/Homework/PHP-W2-HW.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<!-- Student Form -->
+<h2>Student Grades Management</h2>
+<!-- Set Form Action & Form Method -->
+<form method="" action="">
+  Student Name: <input type="text" name="student_name">
+  Grade: <input type="text" name="student_grade">
+  <input type="submit" value="Add Student">
+</form>
+
+<!-- Handle Form Submission & Manage Data -->
+<?php 
+
+// TODO: Capture data
+
+// TODO: Store data in array
+
+// TODO: Set global variables
+
+// TODO: Track total number of students & sum of grades
+
+?>
+
+<!-- Display Student Data & Calculate Grades -->
+
+<?php
+
+// TODO: Display Data
+
+// TODO: Calculate Average Grade
+
+// TODO: Calculate Highest Grade
+
+// TODO: Calculate Lowest Grade
+
+// TODO: Display Grades Data
+
+?>
+
+</body>
+</html>
